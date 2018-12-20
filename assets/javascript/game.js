3eb0d9a4ef17c634d044b28b770514b04e64d97d
Nate Micinski <nmicinski@gmail.com>
2018-12-19 19:48:12 -0800

    add displayLettersPressed function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,6 +15,15 @@ Global Variables
 // Word Bank
 var wordBank = ["droid", "hoth", "tauntaun"];
 
+// Letter Pressed by User
+var letterPressed;
+
+// HTML Elements
+var lettersGuessed = document.getElementById("letters-guessed");
+
+// New Div
+var newDiv = document.createElement("div");
+
 /*
 ======================================
 Event Listeners
@@ -26,11 +35,16 @@ Event Listeners
 //     document.write("Let's get started");
 // }
 
+
 // Listen for Key Press (A-Z Only) | Store Letter in Lowercase
-document.onkeyup = function (event) {
+document.onkeyup = function() {
     if (event.keyCode >= 65 && event.keyCode <= 90) {
-        var code = String.fromCharCode(event.keyCode).toLowerCase();
-        console.log(code);
+        var keyPressed = String.fromCharCode(event.keyCode).toLowerCase();
+        letterPressed = keyPressed;
+        console.log(letterPressed);
+
+        // Display Letters Pressed
+        displayLettersPressed(keyPressed, lettersGuessed);
     };
 };
 
@@ -40,6 +54,22 @@ Function Declarations
 ======================================
 */
 
+// Make a for loop that gets the varible from the array, finds out how long it is, and appends the number of <p> "_"s that are equal to it
+
+// Display String of Letters Pressed
+function displayLettersPressed(letter, str) {
+    lettersGuessed.innerHTML += letter;
+}
+
+// // working on a  version of of displayLettersPressed that doesn't allow repeat letter presses...
+// function displayLettersPressed(letter, str) {
+//     for (var i = 0; i < str.length; i++) {
+//         if (str.charAt(i) != letter) {
+//             lettersGuessed.innerHTML += letter;
+//         }
+//     }
+// }
+
 
 /*
 ======================================
@@ -47,4 +77,4 @@ Function Calls
 ======================================
 */
 
-// keyPressed();
+// console.log(code);
